894191c54dd3c7ba71496c4baadb2cc0e890d1da
Pepito <juanitoperez@gmail.com>
2026-01-16 16:32:15 +0000

    Creados metodos conversionCategoria y cargar

--- src/Persistencia/GestorFicheros.java
+++ src/Persistencia/GestorFicheros.java
@@ -1,55 +1,6 @@
 package Persistencia;
 
-import Modelo.Producto;
-import Utilidades.Utilidades;
-
-import java.io.*;
-import java.util.ArrayList;
-
 import Modelo.Categoria;
-import Modelo.Producto;
-
-import java.io.*;
-import java.util.ArrayList;
-
-
-<<<<<<< HEAD
-import Modelo.Categoria;
-import Modelo.Producto;
-
-import java.io.*;
-import java.util.ArrayList;
-
-
-public class GestorFicheros {
-    public Categoria conversionCategoria(String categoria){
-        if (categoria.equals("ELECTRONICAS")){
-            return Categoria.ELECTRONICAS;
-        }
-        else if (categoria.equals("ALIMENTACION")){
-            return Categoria.ALIMENTACION;
-        }
-        else if (categoria.equals("HOGAR")) {
-            return Categoria.HOGAR;
-        }
-        else {
-            return Categoria.OTROS;
-        }
-    }
-
-    public ArrayList<Producto> cargar(String archivo) throws IOException {
-        BufferedReader bReader = new BufferedReader(new FileReader(archivo));
-        String linea;
-        String[] partes = new String[5];
-        ArrayList <Producto> productos = new ArrayList<>();
-
-        while ((linea = bReader.readLine()) !=null){
-            partes = linea.split(";");
-            //productos.add(new Producto(Integer.parseInt(partes[0]), partes[1], Double.parseDouble(partes[2]), Integer.parseInt(partes[3]), conversionCategoria(partes[4])));
-        }
-        return productos;
-    }
-=======
 import Modelo.Producto;
 import Utilidades.Utilidades;
 
@@ -58,21 +9,6 @@ import java.util.ArrayList;
 
 public class GestorFicheros {
 
-    public void escribirFichero(ArrayList<Producto> productos) {
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter("Inventario.csv"))){
-            for (Producto p : productos) {
-                writer.write(p.toCSV());
-                writer.newLine();
-            }
-        }catch(Exception e){
-            Utilidades.mostrar("Error al escribir el fichero");
-        }
-    }
-    public void p(){
-
-    }
-
->>>>>>> origin/main
     public Categoria conversionCategoria(String categoria){
         if (categoria.equals("ELECTRONICAS")){
             return Categoria.ELECTRONICAS;
@@ -109,8 +45,4 @@ public class GestorFicheros {
             Utilidades.mostrar("Error al escribir el fichero");
         }
     }
-    public void p(){
-
-    }
-
 }
